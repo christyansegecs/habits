6afcfe5869f03d128c7924bd8c3ffb1554d58060
christyansegecs <christyansegecs@hotmail.com>
2024-05-05 03:49:27 -0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+
+import type { Metadata } from 'next'
+import { Dosis, Inter } from 'next/font/google'
+import './globals.css'
+import Image from 'next/image';
+
+const dosis = Dosis({ subsets: ['latin'], variable: '--font-dosis' })
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+
+export const metadata: Metadata = {
+  title: 'Meta Diária - Gerenciador de Hábitos e Tarefas',
+  description: 'Gerencie seus hábitos'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${dosis.variable} ${inter.variable}  flex items-center flex-col mt-10 bg-neutral-900`}>
+        <Image src='/images/logo.svg' width={200} height={200} alt='Logo - meta diária' />
+        {children}
+      </body>
+    </html>
+  )
+}
